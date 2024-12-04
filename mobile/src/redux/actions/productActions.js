be657060ba40beda1c5246c617bfc475e3fcdb7f
miryan1ez <veeveepeexd@gmail.com>
2024-12-04 14:12:41 +0800

    product update fix

--- mobile/src/redux/actions/productActions.js
+++ mobile/src/redux/actions/productActions.js
@@ -5,8 +5,9 @@ export const getAllProducts = (keyword = "", category = "") => async (dispatch) 
     try {
         dispatch({ type: "ALL_PRODUCTS_REQUEST" });
 
+
         const { data } = await axios.get(`${server}/product/all?keyword=${keyword}&category=${category}`);
-        // console.log("product data:", data)
+        console.log("product data:", data)
         dispatch({
             type: "ALL_PRODUCTS_SUCCESS",
             payload: data.products,
@@ -24,7 +25,9 @@ export const getProductsByCategory = (categoryId) => async (dispatch) => {
     try {
         dispatch({ type: "ALL_PRODUCTS_REQUEST" });
 
+
         const { data } = await axios.get(`${server}/product/category/${categoryId}`);
+
 
         dispatch({
             type: "ALL_PRODUCTS_SUCCESS",
@@ -41,17 +44,20 @@ export const getProductsByCategory = (categoryId) => async (dispatch) => {
 export const getProductDetails = (id) => async (dispatch) => {
     try {
         console.log("Fetching product details for ID:", id);  // Log the product ID
-        
+       
         dispatch({
             type: "getProductDetailsRequest",
         });
+
 
         // Fetch product details from the server
         const { data } = await axios.get(`${server}/product/details/${id}`, {
             withCredentials: true,
         });
 
+
         console.log("Product details fetched:", data);  // Log the fetched data
+
 
         dispatch({
             type: "getProductDetailsSuccess",
@@ -60,6 +66,7 @@ export const getProductDetails = (id) => async (dispatch) => {
     } catch (error) {
         // Log error to inspect what went wrong
         console.error("Error fetching product details:", error);
+
 
         // Dispatch failure action with error message
         dispatch({
@@ -73,11 +80,14 @@ export const searchProducts = (keyword) => async (dispatch) => {
     try {
         dispatch({ type: "searchProductsRequest" });
 
+
         const { data } = await axios.get(`${server}/product/search?keyword=${keyword}`, {
             withCredentials: true,
         });
 
+
         console.log("Fetched products data:", data); // Log fetched data
+
 
         dispatch({
             type: "searchProductsSuccess",
@@ -95,10 +105,10 @@ export const updateProduct = (productData) => async (dispatch) => {
     console.log("update touched");
     try {
       dispatch({ type: "UPDATE_PRODUCT_REQUEST" });
-  
+ 
       const { data } = await axios.put(`${server}/product/update/${productData.id}`, productData);
       console.log("update product: ", data);
-  
+ 
       dispatch({
         type: "UPDATE_PRODUCT_SUCCESS",
         payload: data.product,
@@ -110,17 +120,16 @@ export const updateProduct = (productData) => async (dispatch) => {
       });
     }
   };
-  
-  
+ 
   // New Product
   export const newProduct = (productData) => async (dispatch) => {
     console.log("Data sent to newProduct action:", productData);
     try {
       dispatch({ type: "NEW_PRODUCT_REQUEST" });
-  
+ 
       const { data } = await axios.post(`${server}/product/new`, productData);
       console.log("API response data:", data); // Log API response
-  
+ 
       if (data && data.product) {
         dispatch({
           type: "NEW_PRODUCT_SUCCESS",
@@ -137,6 +146,4 @@ export const updateProduct = (productData) => async (dispatch) => {
       });
     }
   };
-  
-
-
+ 
